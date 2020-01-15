2a169b2ede33a7a96dcd67e7c54f375c9f8ff0f5
Nasra <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-01-15 07:09:53 -0800

    finished get requests

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,4 +6,49 @@ const Posts = require("../data/db.js");
 
 router.get("/", (req, res) => {
     Posts.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: "The posts information could not be retrieved." 
+        })
+    })
 })
+
+router.get("/:id", (req, res) => {
+    const id = req.param.id;
+    Posts.findById(id)
+    .then(post => {
+        if (post.id !== 0) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: "The post information could not be retrieved."
+        })
+    })
+})
+
+router.get("/:id/comments", (req, res) => {
+    const id = req.params.id;
+    Posts.findPostComments(id)
+    .then(comments => {
+        if (comments.id !== 0){
+            res.status(200).json(comments)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The comments information could not be retrieved." })
+    })
+})
+
+module.exports = router;
