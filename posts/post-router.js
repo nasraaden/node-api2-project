1310d9831c8095bc55be835de48f6f29bf0757d0
Nasra <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-01-15 07:40:17 -0800

    finished put and delete requests

--- posts/post-router.js
+++ posts/post-router.js
@@ -4,6 +4,15 @@ const router = express.Router();
 
 const Posts = require("../data/db.js");
 
+router.post("/", (req, res) => {
+    const postData = req.body;
+    if (!postData.title || !postData.contents){
+        res.status(200).json( { errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Posts.insert(postData)
+    }
+})
+
 router.get("/", (req, res) => {
     Posts.find()
     .then(posts => {
@@ -11,9 +20,7 @@ router.get("/", (req, res) => {
     })
     .catch(err => {
         console.log(err)
-        res.status(500).json({
-            error: "The posts information could not be retrieved." 
-        })
+        res.status(500).json({ error: "The posts information could not be retrieved." })
     })
 })
 
@@ -29,9 +36,7 @@ router.get("/:id", (req, res) => {
     })
     .catch(err => {
         console.log(err)
-        res.status(500).json({
-            error: "The post information could not be retrieved."
-        })
+        res.status(500).json({ error: "The post information could not be retrieved." })
     })
 })
 
@@ -49,6 +54,43 @@ router.get("/:id/comments", (req, res) => {
         console.log(err)
         res.status(500).json({ error: "The comments information could not be retrieved." })
     })
+})
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    Posts.remove(id)
+    .then(deleted => {
+        if (deleted) {
+            res.status(200).json(deleted)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The post could not be removed." })
+    })
+})
+
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const postData = req.body;
+    if (!postData.title || !postData.contents){
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Posts.update(id, postData)
+        .then(updated => {
+            if (updated){
+                res.status(200).json(updated)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "The post information could not be modified." })
+        })
+    }
 })
 
 module.exports = router;
